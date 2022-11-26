1413d9b416f7d61b49ad7291f80381aa33c620cc
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2022-11-26 10:11:06 +0700

    facebook login not work

--- app/src/main/java/com/aljebrastudio/islamicstory/login/LoginActivity.kt
+++ app/src/main/java/com/aljebrastudio/islamicstory/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.aljebrastudio.islamicstory.login
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -13,10 +14,16 @@ import com.aljebrastudio.islamicstory.databinding.ActivityLoginBinding
 import com.aljebrastudio.islamicstory.forgotpassword.ForgotPasswordActivity
 import com.aljebrastudio.islamicstory.main.MainActivity
 import com.aljebrastudio.islamicstory.register.RegisterActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.GoogleAuthProvider
 import org.koin.android.ext.android.inject
 
@@ -25,15 +32,17 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val loginViewModel by inject<LoginViewModel>()
     private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        callbackManager = CallbackManager.Factory.create()
         initGoogleSignIn()
         onClick()
-
+        loginFacebook()
     }
 
     private fun initGoogleSignIn() {
@@ -121,7 +130,34 @@ class LoginActivity : AppCompatActivity() {
             forgotPassword.setOnClickListener {
                 startActivity(Intent(this@LoginActivity, ForgotPasswordActivity::class.java))
             }
+            buttonFacebook.setOnClickListener {
+                LoginManager.getInstance().logInWithReadPermissions(this@LoginActivity, callbackManager,listOf("public_profile, email"))
+            }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode,resultCode, data)
+    }
+
+    private fun loginFacebook() {
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onCancel() {
+                    Log.e("cancel","Fail To Login")
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.e("error","Fail To Login: ${error.message}")
+                }
+
+                override fun onSuccess(result: LoginResult) {
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                    finishAffinity()
+                }
+
+            })
     }
 
     private fun validationCheck(email: String, pass: String): Boolean {
